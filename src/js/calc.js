4d5c1db664c07507d8bbf57138b64e14d5f4d0de
bazi <vazha.davituliani@gmail.com>
2020-07-12 17:16:42 +0400

    travel calculator

--- src/js/calc.js
+++ src/js/calc.js
@@ -19,8 +19,6 @@ window.addEventListener('DOMContentLoaded', function () {
     resultTaxes.innerHTML = 0;
 
 
-
-
     income.addEventListener('change', function () {
         incomeSum = +this.value;
         total = (incomeSum - outcomeSum) * 12;
@@ -56,6 +54,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+
     checkTaxes.addEventListener('change', function (event) {
         if (event.target.checked) {
             totalTax = total - (total * 20 / 100);
@@ -64,4 +63,75 @@ window.addEventListener('DOMContentLoaded', function () {
             resultTaxes.innerHTML = 0;
         }
     });
+
+    //result
+    let gettravel = document.getElementById('totaltravel'); // total prise
+    let gettravelers = document.getElementById('totalper'); // prise per traveler
+
+    //form
+    let travelersSum = document.getElementById('travelerssum'); // amount of travelers
+    let daysSum = document.getElementById('dayssum'); // amount of days
+    let countriessum = document.getElementById('countries'); // all countries
+    let travelersFinal = 0;
+    let daysFinal = 0;
+    let priseAll = 0;
+    let priseAllPersons = 0;
+
+    gettravel.innerHTML = 0;
+    gettravelers.innerHTML = 0;
+
+
+    travelersSum.addEventListener('change', function () {
+        travelersFinal = +this.value;
+        priseAll = (travelersFinal + daysFinal) * 1000;
+        priseAllPersons = priseAll / daysFinal;
+
+        if (daysSum.value == '') {
+            gettravel.innerHTML = 0;
+            gettravelers.innerHTML = 0;
+        } else if (travelersSum.value == '') {
+            gettravel.innerHTML = 0;
+            gettravelers.innerHTML = 0;
+        } else {
+            gettravel.innerHTML = priseAll;
+            gettravelers.innerHTML = priseAllPersons;
+        }
+
+    });
+
+
+    daysSum.addEventListener('change', function () {
+        daysFinal = +this.value;
+        priseAll = (travelersFinal + daysFinal) * 1000;
+        priseAllPersons = priseAll / daysFinal;
+
+        if (daysSum.value == '') {
+            gettravel.innerHTML = 0;
+            gettravelers.innerHTML = 0;
+        } else if (travelersSum.value == '') {
+            gettravel.innerHTML = 0;
+            gettravelers.innerHTML = 0;
+        } else {
+            gettravel.innerHTML = priseAll;
+            gettravelers.innerHTML = priseAllPersons;
+        }
+
+    });
+
+    countries.addEventListener('change', function () {
+        if (travelersSum.value == '' || daysSum.value == '') {
+            gettravel.innerHTML = 0;
+            gettravelers.innerHTML = 0;
+        } else {
+            let per = priseAll;
+            let persons = priseAllPersons;
+            gettravel.innerHTML = per * this.options[this.selectedIndex].value;
+            gettravelers.innerHTML = persons * this.options[this.selectedIndex].value;
+        }
+    });
+
+
+
+
+
 });
--- src/js/main.js
+++ src/js/main.js
@@ -34,4 +34,8 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     })
 
+
+
+
+
 });
