489a063e93a3deafa39141437f1eb8e97e7059d6
bazi <vazha.davituliani@gmail.com>
2020-07-20 16:44:47 +0400

    create Timer using pure Javascript

--- src/js/form.js
+++ src/js/form.js
@@ -3,6 +3,8 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
 
+    // Form 1 (with JSON and callback function)
+
     let notifications = {
         loading: 'loading...',
         success: 'Thanks, we will contact with you soon',
@@ -43,7 +45,6 @@ window.addEventListener('DOMContentLoaded', function () {
             let json = JSON.stringify(object);
 
 
-
             request.send(json);
 
             request.addEventListener('readystatechange', function () {
@@ -73,6 +74,97 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
     });
+
+
+
+    // Form 2 (with JSON and promises)
+
+
+    let warnings = {
+        loading: 'loading...',
+        success: 'Assistance will be provided for you shortly',
+        failure: 'something went wrong, try again later',
+    };
+
+    let forms = document.querySelector('.form__second');
+    let inputs = document.getElementsByTagName('input');
+    let inputy = document.getElementById('formsecond');
+    let statusMessage = document.createElement('div');
+    let textareas = document.getElementsByTagName('textarea');
+    statusMessage.classList.add('status');
+
+
+
+
+    forms.addEventListener('submit', function (events) {
+
+        events.preventDefault();
+        forms.appendChild(statusMessage);
+
+        if (inputy.value.length == '') {
+            statusMessage.innerHTML = warnings.failure;
+        } else {
+
+            function sendForm() {
+
+                return new Promise(function (resolve, reject) {
+
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'http://localhost/local/server.php');
+                    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+                    let formData = new FormData(forms);
+
+                    let obj = {};
+
+                    formData.forEach(function (value, key) {
+                        obj[key] = value;
+                    });
+
+                    let json = JSON.stringify(obj);
+
+                    request.send(json);
+
+                    request.addEventListener('readystatechange', function () {
+                        if (request.readyState < 4) {
+                            resolve()
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve()
+                        } else {
+                            reject()
+                        }
+                    });
+
+                });
+
+            }
+
+            function clearForm() {
+                for (let i = 0; i < inputs.length; i++) {
+                    inputs[i].value = '';
+
+                }
+            }
+
+            function clearArea() {
+                for (let i = 0; i < textareas.length; i++) {
+                    textareas[i].value = '';
+                }
+            }
+
+
+
+            sendForm()
+                .then(() => statusMessage.innerHTML = warnings.loading)
+                .then(() => statusMessage.innerHTML = warnings.success)
+                .catch(() => statusMessage.innerHTML = warnings.failure)
+                .then(clearForm)
+                .then(clearArea)
+
+        }
+
+    });
+
+
 
 
 });
---
+++ src/js/timer.js
@@ -0,0 +1,54 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+
+    let deadline = '2020-12-31';
+
+
+
+    function getDate(finaldate) {
+        let time = Date.parse(finaldate) - Date.parse(new Date());
+        let seconds = Math.floor((time / 1000) % 60);
+        let minutes = Math.floor((time / 1000 / 60) % 60);
+        let hours = Math.floor((time / 1000 / 60 / 60) % 24);
+        let days = Math.floor((time / (1000 * 60 * 60 * 24)));
+
+
+        return {
+            'total': time,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours,
+            'days': days
+        };
+
+    }
+
+    function setTime(id, endtime) {
+        let timer = document.getElementById(id);
+        let days = timer.querySelector('.days');
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(update, 1000);
+
+        function update() {
+            let times = getDate(endtime);
+            days.innerHTML = times.days;
+            hours.innerHTML = times.hours;
+            minutes.innerHTML = times.minutes;
+            seconds.innerHTML = times.seconds;
+
+
+            if (times.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+
+    }
+
+    setTime('timer', deadline);
+
+
+});
